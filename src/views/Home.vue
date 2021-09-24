ee4cc0fb5ca524558be5a4bf70a80ff8c622dee9
wickieonya <wickieonya@gmail.com>
2021-09-24 21:02:11 +0300

    added home plans

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
   <span>
     <home-hero/>
     <home-details/>
+    <home-plans/>
   </span>
 </template>
 
 <script>
 import HomeHero from "@/components/HomeHero";
 import HomeDetails from "@/components/HomeDetails";
+import HomePlans from "@/components/HomePlans";
 
 export default {
   name: 'Home',
 
-  components: {HomeDetails, HomeHero},
+  components: {HomePlans, HomeDetails, HomeHero},
 }
 </script>
